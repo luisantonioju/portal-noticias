ba0dd47cd9b6449884de31f55332bf0cefa3a658
LuisAntonioJu <luisantoniodeassisjunior@gmail.com>
2026-06-11 14:43:00 -0300

    Criação da tabela Categoria

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -43,7 +43,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuarios');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
